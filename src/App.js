9ba90d53fe57aac90c1c57af673e2b23b45bd79b
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2017-05-15 16:55:41 -0400

    Use white logo for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../public/img/logos/L -logo-black.png';
+import logo from '../public/img/logos/L -logo-white.png';
 import './App.css';
 
 class App extends Component {
