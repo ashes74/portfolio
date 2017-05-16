b6a0d78c80abee91768a742d10342786eca023d0
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2017-05-16 13:04:33 -0400

    Add some bio.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo_black from '../public/img/logos/L -logo-black.png'
 import './styles/App.css';
 import Button from './components/Button.jsx'
@@ -8,20 +8,30 @@ class App extends Component {
     return (
       <div className="App">
         <div className="id-panel">
-          <img src={logo_black} alt="LA logo"/> 
-       </div>
-       <div className="content-panel">
-         <p>
-         Hi! 
-
-         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ab perspiciatis repudiandae esse numquam nihil obcaecati, officiis deserunt aspernatur beatae sunt. Ipsam debitis labore, deserunt, libero rerum laborum ea quibusdam.
-         </p>
-         <div className="buttons">
+          <img src={logo_black} alt="LA logo"/>
+        </div>
+        <div className="content-panel">
+          <p>
+            Hi! I'm Lori
+          </p>
+          <p>
+            I am a fullstack software engineer (mostly Javascript) and budding project manager and developer evangelist.
+          </p>
+          <p>
+            I work for include.io ( in beta) and travel regularly to promote our product and
+            learn from users the best ways to improve.
+          </p>
+          <p>
+            I LOVE soccer! I used to play almost every Sunday for up to 3 hours (looking for
+            a new team). I enjoy screaming at the TV during Champions League, La Liga and
+            Europa cup matches.
+          </p>
+          <div className="buttons">
             <Button content="Projects"/>
             <Button content="Resume"/>
-            <Button content="Contact" />
-         </div>
-       </div>
+            <Button content="Contact"/>
+          </div>
+        </div>
       </div>
     );
   }
