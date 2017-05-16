77397804a821daf11f644b710dae19dbbef3b649
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2017-05-16 15:24:38 -0700

    Data for project detail

---
+++ src/assets/projects.js
@@ -0,0 +1,56 @@
+export default [
+    {
+      "name": "Miss Direction",
+      "description": "A mapping engine that misguides you",
+      "highlights": [
+        "Won award at AIHacks 2016",
+        "Built by all women team of newbie programmers"
+      ],
+      "keywords": [
+        "GoogleMaps", "Chrome Extension", "Javascript"
+      ],
+      "url": "http://ashes74.github.io/miss-direction/",
+      "team": ["Galen Corey", "Dani YoungSmith"]
+
+    },
+    {
+      "name": "Path Paradox",
+      "description": "Capstone project: A fullstack online multiplayer game based on the board game Tsuro",
+      "highlights": [
+        "Worked primarily on game logic, architecture and on code reviews"
+      ],
+        "keywords": [
+          "AngularJS", "AngularFire", "Firebase", "lodash", "Gulp"
+        ],
+        "url": "http://pathparadox.com/",
+        "team": ["Kimber Lynch", "Jennifer Cen", "Taffy Chen"]
+
+      },{
+        "name": "Mrs. Pac",
+        "description": "An exercise in pathfinding AI algorithms with a beloved oldie",
+        "highlights":[
+          "Uses the Djikstra's pathfinding algorithm to locate the Pac character in a maze"
+        ],
+        "keywords":
+        ["Javascript", "HTML", "CSS"],
+        "url": "TBD",
+        "team": ["Me"]
+
+      }
+    
+    ]
+
+
+
+// schema:
+//     name: PropTypes.string.isRequired,
+//     description: PropTypes.string.isRequired,
+//     highlights: PropTypes.arrayOf(PropTypes.string),
+//     keywords: PropTypes.arrayOf(PropTypes.string),
+//     startDate: PropTypes.string,
+//     endDate: PropTypes.string,
+//     url: PropTypes.string,
+//     roles: PropTypes.arrayOf(PropTypes.string),
+//     entity: PropTypes.string, // Company/entity affiliations e.g. 'greenpeace', 'corporationXYZ'"
+//     type: PropTypes.string, //" e.g. 'volunteering', 'presentation', 'talk', 'application', 'conference'"
+//     thumbnail: PropTypes.string
