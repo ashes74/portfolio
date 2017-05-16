3db0884471a9fd1f08e57a85225f1cfdaed47c17
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2017-05-16 12:55:42 -0400

    Add new button to interface

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo_black from '../public/img/logos/L -logo-black.png'
 import './styles/App.css';
+import Button from './components/Button.jsx'
 
 class App extends Component {
   render() {
@@ -16,9 +17,9 @@ class App extends Component {
          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus ab perspiciatis repudiandae esse numquam nihil obcaecati, officiis deserunt aspernatur beatae sunt. Ipsam debitis labore, deserunt, libero rerum laborum ea quibusdam.
          </p>
          <div className="buttons">
-           <button> Projects </button>
-           <button> Resume </button>
-           <button> Contact </button>
+            <Button content="Projects"/>
+            <Button content="Resume"/>
+            <Button content="Contact" />
          </div>
        </div>
       </div>
